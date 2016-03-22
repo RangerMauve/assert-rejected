5bf90cc64c0f1edbbb1b4b79f9ccbb03d65f2f47
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2016-03-22 16:34:59 +0700

    Add test

--- test.js
+++ test.js
@@ -5,6 +5,13 @@ test("rejected promises should pass", function(t) {
 	return assertRejected(Promise.reject(new Error("Test")));
 });
 
+test("rejected promises should resolve with rejection", function(t) {
+	var error = new Error("Test");
+	return assertRejected(Promise.reject(error)).then(function (x) {
+		if (x !== error) throw new Error("Rejected error not sent to .then(). Received: " + x);
+	});
+});
+
 test("message comparisons are made if specified", function(t) {
 	return assertRejected(Promise.reject(new Error("Test")), "Test");
 });
